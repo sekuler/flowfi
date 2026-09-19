8627487fa8d692aa348085e9e54f276ed7d335ca
sekuler <kazimmarmara16@gmail.com>
2026-09-19 23:42:30 +0300

    FlowFi update

--- src/App.tsx
+++ src/App.tsx
@@ -478,8 +478,8 @@ function AppInner() {
           </div>
         </div>
 
-        <div style={{ flex: "1 1 380px", minWidth: 280, display: "flex", justifyContent: "center" }}>
-          <img src="/usdc-hero.jpg" alt="USDC on Arc" style={{ width: "100%", maxWidth: 460, height: "auto", borderRadius: 24 }} />
+        <div style={{ flex: "1 1 460px", minWidth: 320, display: "flex", justifyContent: "center" }}>
+          <img src="/usdc-hero.jpg" alt="USDC on Arc" style={{ width: "100%", maxWidth: 620, height: "auto", borderRadius: 0, mixBlendMode: "multiply" }} />
         </div>
       </div>
 
